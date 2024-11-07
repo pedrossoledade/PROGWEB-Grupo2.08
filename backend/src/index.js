4dddd45430e4f4c18fccfb28980a3d35bb3868bc
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-07 19:55:31 -0400

    (test): configuracao para testar a conexao quando iniciar o servidor

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const testconnection = require('./prisma/testConnection.js');
 
 const PORT = 3000;
 const HOST = '0.0.0.0';
 
 const app = express();
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  const msg = 'Hello World';
+  res.send(msg);
 });
+
+
 
 app.listen(PORT, HOST);
