4e3e2ac02c582db3c25cb9dc365fa908e86f73c1
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-10 01:55:56 -0400

    feat: configure environment variables and set up user routes in the server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,24 @@
 const express = require('express');
-const testconnection = require('./prisma/testConnection.js');
+const dotenv = require('dotenv');
+const testConnection = require('./prisma/testConnection.js');
+const userRoutes = require('./routes/userRoutes.js');
 
-const PORT = 3000;
-const HOST = '0.0.0.0';
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
 
-
+// Usando as rotas de usuário
+app.use('/user', userRoutes);
 
 app.get('/', (req, res) => {
   const msg = 'Hello World';
   res.send(msg);
 });
 
-
-
-app.listen(PORT, HOST);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
