a1f2194befde7831aa8d7a90aa55f5e28a09323e
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-13 01:55:54 -0400

    refactor: separacao dos controllers dos routes

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,41 +1,12 @@
 const express = require('express');
-const userService = require('../services/userService');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
-const dotenv = require('dotenv');
+router.post('/register', userController.register);
 
-dotenv.config();
+router.post('/login', userController.login);
 
-const prisma = new PrismaClient();
-
-// Chave secreta para assinar o token JWT
-const JWT_SECRET = process.env.JWT_SECRET;
-
-router.post('/register', async (req, res) => {
-    try {
-        const result = await userService.register(req.body);
-        res.status(201).json(result);
-    } catch (error) {
-        console.error('Error registering user:', error);
-        res.status(error.message.includes('já existe') ? 400 : 500)
-           .json({ message: error.message || 'Internal server error' });
-    }
-});
-
-router.post('/login', async (req, res) => {
-    try {
-        const result = await userService.login(req.body);
-        res.json(result);
-    } catch (error) {
-        console.error('Error logging in:', error);
-        res.status(error.message.includes('obrigatório') ? 400 : 401)
-           .json({ message: error.message || 'Internal server error' });
-    }
-});
 
 module.exports = router;
