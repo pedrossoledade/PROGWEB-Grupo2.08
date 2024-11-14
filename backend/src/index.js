743f2e4672a460cc0e3a4cf67eb75aa0438111d8
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-14 19:50:25 -0400

    feat: update server port configuration to use PORT_APP environment variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,7 @@ const userRoutes = require('./routes/userRoutes.js');
 // Carregar variáveis de ambiente do arquivo .env
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT_APP || 3000;
 
 const app = express();
 app.use(express.json());
