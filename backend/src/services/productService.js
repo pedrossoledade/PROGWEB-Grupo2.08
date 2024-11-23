6653df389258264cf02fad1e45472ea4a00b8c0f
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-22 21:48:02 -0400

    feat: implentacao do ProductService

---
+++ backend/src/services/productService.js
@@ -0,0 +1,41 @@
+const productRepository = require("../repository/productRepository");
+const ProductRepository = require("../repository/productRepository");
+
+class ProductService{
+
+    async getAllProduct() {
+        return await ProductRepository.findAll();
+    }
+
+    async getProductById(id) {
+        return await ProductRepository.findById(id);
+    }
+
+    async getProductByName(productName){
+        return await ProductRepository.findByName(productName);
+    }
+
+    async getProductByCategory(category){
+        return await productRepository.findByCategory();
+    }
+
+    async getProductByCategoryId(categoryId){
+        return await productRepository.findByCategory();
+    }
+
+
+    async createProduct(productData){
+        return await ProductRepository.create( productData);
+    }
+
+    async updateProduct(id, productData){
+        return await ProductRepository.update(id, productData);
+    }
+
+    async deleteProduct(id){
+        return await ProductRepository.delete(id);
+    }
+    
+}
+
+module.exports = new ProductRepository();
