0a6f501b21bf68a1cb0e3d371c6a4c82a4c21944
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-22 22:51:01 -0400

    feat: add productRoutes for managing product-related endpoints

---
+++ backend/src/routes/productRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const ProductController = require('../controllers/ProductController');
+
+// Rota para buscar todos os produtos
+router.get('/products', ProductController.getAllProducts);
+
+// Rota para buscar um produto pelo ID
+router.get('/products/:id', ProductController.getProductById);
+
+// Rota para buscar produtos pelo nome
+router.get('/products/name/:name', ProductController.getProductByName);
+
+// Rota para buscar produtos por nome de categoria
+router.get('/products/category/:category', ProductController.getProductByCategory);
+
+// Rota para buscar produtos pelo ID da categoria
+router.get('/products/categoryId/:categoryId', ProductController.getProductByCategoryId);
+
+// Rota para criar um novo produto
+router.post('/products', ProductController.createProduct);
+
+// Rota para atualizar um produto existente
+router.put('/products/:id', ProductController.updateProduct);
+
+// Rota para deletar um produto
+router.delete('/products/:id', ProductController.deleteProduct);
+
+module.exports = router;
