fd5df077efab3eaba4c607beac6c4edf00c90f0a
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-24 13:09:27 -0400

    refactor: update productRoutes to simplify endpoint paths

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -3,27 +3,27 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 // Rota para buscar todos os produtos
-router.get('/products', productController.getAllProducts);
+router.get('/', productController.getAllProducts);
 
 // Rota para buscar um produto pelo ID
-router.get('/products/:id', productController.getProductById);
+router.get('/:id', productController.getProductById);
 
 // Rota para buscar produtos pelo nome
-router.get('/products/name/:name', productController.getProductByName);
+router.get('/name/:name', productController.getProductByName);
 
 // Rota para buscar produtos por nome de categoria
-router.get('/products/category/:category', productController.getProductByCategory);
+router.get('/findByCategory/:category', productController.getProductByCategory);
 
 // Rota para buscar produtos pelo ID da categoria
-router.get('/products/categoryId/:categoryId', productController.getProductByCategoryId);
+router.get('/findByCategoryId/:categoryId', productController.getProductByCategoryId);
 
 // Rota para criar um novo produto
-router.post('/products', productController.createProduct);
+router.post('/', productController.createProduct);
 
 // Rota para atualizar um produto existente
-router.put('/products/:id', productController.updateProduct);
+router.put('/:id', productController.updateProduct);
 
 // Rota para deletar um produto
-router.delete('/products/:id', productController.deleteProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
