f618461d8e4b6181cb34824bab3dbffcd0ca85fc
Kauan Cardoso <kauancardososilva@gmail.com>
2024-11-26 14:41:40 -0400

    feat: implement session management in user registration and login, and add logout functionality

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -5,7 +5,8 @@ class UserController {
     async register(req, res){
         try {
             const result = await userService.register(req.body);
-            res.status(201).json(result);
+            req.session.userId = result.user.id; // Salvar o ID do usuário na sessão
+            res.status(201).json({ message: 'Usuário registrado com sucesso' });
         } catch (error) {
             console.error('Error registering user:', error);
             res.status(error.message.includes('já existe') ? 400 : 500)
@@ -16,15 +17,23 @@ class UserController {
     async login(req, res){
         try {
             const result = await userService.login(req.body);
-            console.log('result', result);
-            res.status(200).json(result); 
+            req.session.userId = result.user.id; // Salvar o ID do usuário na sessão
+            res.status(200).json({ message: 'Login realizado com sucesso' });
         } catch (error) {
             console.error('Error logging in:', error);
             res.status(error.message.includes('obrigatório') ? 400 : 401)
                .json({ message: error.message || 'Internal server error' });
         }
     }
+
+    async logout(req, res) {
+        req.session.destroy(err => {
+            if (err) {
+                return res.status(500).json({ message: 'Erro ao fazer logout' });
+            }
+            res.status(200).json({ message: 'Logout realizado com sucesso' });
+        });
+    }
 }
 
 module.exports = new UserController();
-
